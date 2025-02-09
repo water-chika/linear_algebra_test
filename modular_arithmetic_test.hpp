9f4b0d1eb742b16fbd7258d8901bfe0e6e69906b
Water Chika <water_chika@outlook.com>
2025-02-09 10:03:32 +0000

    Add simple tests for modular_arithmetic

--- modular_arithmetic_test.hpp
+++ modular_arithmetic_test.hpp
@@ -1,15 +1,20 @@
 #pragma once
+
+#include <functional>
+
 #include <modular_arithmetic.hpp>
 
+#include "test.hpp"
+
 bool test_modular_arithmetic() {
-    using m_t = linear_algebra::modular_arithmetic<size_t, 3>;
     auto passed = true;
-    m_t a{1}, b{2};
-    auto c = a+b;
-    passed = passed && c == 0;
-    auto d = a-b;
-    passed = passed && d == 2;
-    auto e = a*b;
-    passed = passed && e == 2;
-    return passed;
+    auto a = linear_algebra::modular_arithmetic{1, linear_algebra::prime_number{std::integral_constant<int,3>{}}};
+    decltype(a) b{2,std::integral_constant<int,3>{}};
+    auto tests = std::vector<std::function<bool()>>{
+        [a,b](){ return static_cast<int>(a+b) == 0; },
+        [a,b](){ return static_cast<int>(a-b) == 2; },
+        [a,b](){ return static_cast<int>(a*b) == 2; },
+        [a,b](){ return static_cast<int>(a/b) == 2; },
+    };
+    return run_simple_tests("modular_arithmetic", tests);
 }
