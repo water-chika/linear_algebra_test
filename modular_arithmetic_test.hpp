169d19babf82122848c731c357a675c62cc7c72e
Water Chika <water_chika@outlook.com>
2025-02-13 17:31:14 +0000

    Redesign modular_arithmetic

--- modular_arithmetic_test.hpp
+++ modular_arithmetic_test.hpp
@@ -8,13 +8,17 @@
 
 bool test_modular_arithmetic() {
     auto passed = true;
-    auto a = linear_algebra::modular_arithmetic{1, linear_algebra::prime_number{std::integral_constant<int,3>{}}};
-    decltype(a) b{2,std::integral_constant<int,3>{}};
+    using modular_arithmetic::mod;
+    using modular_arithmetic::divides;
+    auto add_mod3 = [](auto lhs, auto rhs) { return mod(lhs+rhs, 3); };
+    auto sub_mod3 = [](auto lhs, auto rhs) { return mod(lhs-rhs, 3); };
+    auto mul_mod3 = [](auto lhs, auto rhs) { return mod(lhs*rhs, 3); };
+    auto div_mod3 = [](auto lhs, auto rhs) { return divides(lhs, rhs, 3); };
     auto tests = std::vector<std::function<bool()>>{
-        [a,b](){ return static_cast<int>(a+b) == 0; },
-        [a,b](){ return static_cast<int>(a-b) == 2; },
-        [a,b](){ return static_cast<int>(a*b) == 2; },
-        [a,b](){ return static_cast<int>(a/b) == 2; },
+        [&add_mod3](){ return add_mod3(1,2) == 0; },
+        [&sub_mod3](){ return sub_mod3(1,2) == 2; },
+        [&mul_mod3](){ return mul_mod3(1,2) == 2; },
+        [&div_mod3](){ return div_mod3(1,2) == 2; },
     };
     return run_simple_tests("modular_arithmetic", tests);
 }
